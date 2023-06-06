0e305eab815cb648be994817ae8e8a28f43a4ef0
Razan <razanalzannan@gmail.com>
2023-06-07 00:15:30 +0300

    Initial Commit

---
+++ ContactsFirebase/ContactsFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsFirebaseApp.swift
+//  ContactsFirebase
+//
+//  Created by Razan Mohammed Alzannan on 18/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ContactsFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
